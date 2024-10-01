d50ed14ec5259aa0c4f4335251910d8e3a245691
Erick-DAS <erickdiogo2004@gmail.com>
2024-10-01 15:49:16 -0300

    ✨ Initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -16,8 +16,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-import app.models
-target_metadata = app.models.base.metadata
+from app.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- app/models.py
+++ app/models.py
@@ -1,4 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+import sqlalchemy as sa
+
+from uuid import uuid4
+
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = sa.Column(sa.UUID, primary_key=True, default=lambda: str(uuid4()))
+    full_name = sa.Column(sa.String)
+    email = sa.Column(sa.String, unique=True)
+    role = sa.Column(sa.String)
