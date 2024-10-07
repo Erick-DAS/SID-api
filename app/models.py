2d30c3bb10cf833e2c5d668bd74e5bc40381aeb7
Erick-DAS <erickdiogo2004@gmail.com>
2024-10-07 17:03:53 -0300

    Create Article and Section tables

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+import sqlalchemy.orm as orm
 
 from uuid import uuid4
 
@@ -13,3 +14,20 @@ class User(Base):
     full_name = sa.Column(sa.String)
     email = sa.Column(sa.String, unique=True)
     role = sa.Column(sa.String)
+
+class Section(Base):
+    __tablename__ = "sections"
+
+    id = sa.Column(sa.UUID, primary_key=True, default=lambda: str(uuid4()))
+    name = sa.Column(sa.String, unique=True, nullable=False)
+    
+    main_article_id = sa.Column(sa.ForeignKey("articles.id"), nullable=True)
+    main_article = orm.relationship("Article", foreign_keys=[main_article_id])
+    
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = sa.Column(sa.UUID, primary_key=True, default=lambda: str(uuid4()))
+    title = sa.Column(sa.String, nullable=False)
+    content = sa.Column(sa.Text, nullable=False)
+    section = sa.Column(sa.Text, nullable=True)
