c5905fa96315a78e54e189956bc4150f8cad1165
Enzo <enzo.neves@usp.br>
2024-10-15 18:43:38 -0300

    adding search api router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+import app.api.search as search_api
 import app.api.user as user_api
 
 app = FastAPI()
 
 app.include_router(user_api.app)
+app.include_router(search_api.app)
