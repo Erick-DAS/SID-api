175c9b58b0803251a0882d907e0c4895584c6d9a
Erick-DAS <erickdiogo2004@gmail.com>
2024-10-29 15:03:20 -0300

    Add vercel deployment json and fix .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
