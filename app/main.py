3b672fc500f4fd378f13cab0cc815d1657ce1ab9
Erick-DAS <erickdiogo2004@gmail.com>
2024-11-05 16:40:57 -0300

    Add CORS support for frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import app.api.article as article_api
 import app.api.user as user_api
@@ -19,7 +20,21 @@ tags_metadata = [
     },
 ]
 
+origins = [
+    "https://sid-webpage.vercel.app",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
 app = FastAPI(debug=True, openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP
+    allow_headers=["*"],
+)
 
 app.include_router(prefix="/users", router=user_api.app, tags=["Users"])
 app.include_router(prefix="/articles", router=article_api.app, tags=["Articles"])
