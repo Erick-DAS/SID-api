d1486cfddf105a5d452ba7205e8d0a049f04bbc3
Erick-DAS <erickdiogo2004@gmail.com>
2024-11-28 13:39:30 -0300

    Create Data route

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import app.api.article as article_api
 import app.api.user as user_api
 import app.api.version as version_api
+import app.api.data as data_api
 
 tags_metadata = [
     {
@@ -17,6 +18,10 @@ tags_metadata = [
     {
         "name": "Versions",
         "description": "_Manage article versions_",
+    },
+    {
+        "name": "Data",
+        "description": "_Access external data_",
     },
 ]
 
@@ -32,10 +37,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Use origins=origins to allow only the specified origins
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos os métodos HTTP
+    allow_methods=["*"],  # Allow every HTTP method
     allow_headers=["*"],
 )
 
 app.include_router(prefix="/users", router=user_api.app, tags=["Users"])
 app.include_router(prefix="/articles", router=article_api.app, tags=["Articles"])
 app.include_router(prefix="/versions", router=version_api.app, tags=["Versions"])
+app.include_router(prefix="/data", router=data_api.app, tags=["Data"])
